3df15c7ca6b5ac8d03663b34ae7f871679b1be9f
Cesar Ramos <caramosd@unah.hn>
2020-09-16 15:56:35 -0600

    Agregando carpeta

---
+++ Ejercicios/Ejercicio1/holamundo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b, suma;
+    cout << "Ingrese el valor de a: "; 
+    cin >> a;
+    cout << "Ingrese el valor de b: ";
+    cin >> b;
+    suma=a+b;
+    cout << "El valor de la suma es = " << suma << "\n";
+    system("PAUSE");
+    return 0;
+}
